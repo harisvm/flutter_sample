ad9b43d93744c648676c764e9a2f40c646b9270e
harry$ <harinarayananvm1692@gmail.com>
2020-05-11 17:25:12 +0530

    animated widget made working

--- lib/AlbumList.dart
+++ lib/AlbumList.dart
@@ -12,37 +12,37 @@ class AlbumList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,mainAxisSpacing: 2,crossAxisSpacing: 2,childAspectRatio: .6
-      ),
+          crossAxisCount: 3,
+          mainAxisSpacing: 2,
+          crossAxisSpacing: 2,
+          childAspectRatio: .6),
       itemCount: albumList.length,
       itemBuilder: (context, index) {
-
-        return GestureDetector(
-
-          onTap: (){
-            setState(){
-
-              selected = !selected;
-            }
-
-          },
-
-        child: AnimatedContainer(
-            width: selected ? 200.0 : 100.0,
-            height: selected ? 100.0 : 200.0,
-
-            alignment:
-            selected ? Alignment.center : AlignmentDirectional.topCenter,
-
-            duration: Duration(seconds: 2),
-            curve: Curves.fastOutSlowIn,
-            color:selected? Colors.primaries[Random().nextInt(Colors.primaries.length)]:Colors.primaries[Random().nextInt(Colors.primaries.length)],
-            child: Text(
-              albumList[index].title,
-              style: TextStyle(color: Colors.white),
-            ))
+        return StatefulBuilder(
+          builder: (context, setState) => GestureDetector(
+              onTap: () {
+                setState((){
+                  selected = !selected;
+                });
+              },
+              child: AnimatedContainer(
+                  width: selected ? 200.0 : 100.0,
+                  height: selected ? 100.0 : 200.0,
+                  alignment: selected
+                      ? Alignment.center
+                      : AlignmentDirectional.topCenter,
+                  duration: Duration(seconds: 2),
+                  curve: Curves.fastOutSlowIn,
+                  color: selected
+                      ? Colors
+                          .primaries[Random().nextInt(Colors.primaries.length)]
+                      : Colors
+                          .primaries[Random().nextInt(Colors.primaries.length)],
+                  child: Text(
+                    albumList[index].title,
+                    style: TextStyle(color: Colors.white),
+                  ))),
         );
       },
     );
