a0856f19f79d1283ba908503a4a78e96bdf4f91e
harry$ <harinarayananvm1692@gmail.com>
2020-05-12 17:04:20 +0530

    navigation drawert added

--- lib/AlbumList.dart
+++ lib/AlbumList.dart
@@ -15,9 +15,10 @@ class AlbumList extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.builder(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 1,
-          mainAxisSpacing: 8,
-          childAspectRatio: 2),
+        crossAxisCount: 1,
+        mainAxisSpacing: 8,
+        childAspectRatio: 3,
+      ),
       itemCount: albumList.length,
       itemBuilder: (context, index) {
         return StatefulBuilder(
@@ -27,39 +28,51 @@ class AlbumList extends StatelessWidget {
                       selected = !selected;
                     });
                   },
-              
-                  child: Card(
-
-                    child: Row(children: <Widget>[
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: FadeInImage.assetNetwork(
-                            placeholder: 'assets/loading.png',
-                            image: albumList[index].url,fit: BoxFit.cover,
-                          )),
-                      Column(
-                        children: <Widget>[
-                          
-
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(5, 20, 10, 5),
-                            child: Text(
-                              'Item',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20),
-                            ),
-                          ),
-                          Padding(
-                              padding: const EdgeInsets.fromLTRB(5, 0, 10, 5),
+                  child: SizedBox(
+                    width: 150,
+                    height: 150,
+                    child: Card(
+                      child: Row(children: <Widget>[
+                        ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: FadeInImage.assetNetwork(
+                              placeholder: 'assets/loading.png',
+                              width: 150,
+                              height: 150,
+                              image: albumList[index].url,
+                              fit: BoxFit.cover,
+                            )),
+                        Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(5, 20, 10, 5),
                               child: Text(
-                                'Description',
+                                'Item',
                                 style: TextStyle(
-                                    fontWeight: FontWeight.normal,
-                                    fontSize: 15),
-                              ))
-                        ],
-                      ),
-                    ]),
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                            ),
+                            Padding(
+                                padding: const EdgeInsets.fromLTRB(5, 0, 10, 5),
+                                child: Text(
+                                  'Description',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 15),
+                                )),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0
+
+
+                                  , 0, 10, 5),
+                              child: IconTheme(
+                                  data: IconThemeData(color: Colors.red),
+                                  child: Icon(Icons.star)),
+                            )
+                          ],
+                        ),
+                      ]),
+                    ),
                   ),
                 ));
       },
--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,18 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/AlbumList.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 
+
 Future<List<Album>> fetchAlbum() async {
-  final response =
-  await http.get('https://picsum.photos/v2/list');
+  final response = await http.get('https://picsum.photos/v2/list');
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
 
     return compute(parsePhotos, response.body);
-
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
@@ -65,37 +65,71 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.green,
       ),
       home: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text("Menu"),
+                decoration: BoxDecoration(color: Colors.blueAccent),
+              ),
+              ListTile(
+                title: Text("Item 1"),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text("Item 1"),
+                onTap: () {},
+              )
+            ],
+          ),
+        ),
         appBar: AppBar(
-          title: Text('Fetch Data From mApi'),
+          backgroundColor: Colors.blueAccent,
+          title: Text('Groceries'),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+                onPressed: (){
+                  Fluttertoast.showToast(msg: "Cart clicked",toastLength: Toast.LENGTH_LONG,
+                gravity: ToastGravity.BOTTOM,timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.green,
+                  textColor: Colors.white,
+                  fontSize: 15.0
+                  );
+                }  )
+
+          ],
         ),
         body: Center(
+          child: SafeArea(
+            child: FutureBuilder<List<Album>>(
+              future: futureAlbum,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return snapshot.hasData
+                      ? AlbumList(albumList: snapshot.data)
+                      : Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
 
-
-          child:SafeArea(child:  FutureBuilder<List<Album>>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-
-
-                return snapshot.hasData
-                    ? AlbumList(albumList: snapshot.data)
-                    : Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-
-              // By default, show a loading spinner.
-              return CircularProgressIndicator();
-            },
+                // By default, show a loading spinner.
+                return CircularProgressIndicator();
+              },
+            ),
+            left: true,
           ),
-       left: true, ),
-      ),
+        ),
       ),
     );
   }
 }
-FutureOr<List<Album>> parsePhotos(String message) {
 
+FutureOr<List<Album>> parsePhotos(String message) {
   final parsed = jsonDecode(message).cast<Map<String, dynamic>>();
 
   return parsed.map<Album>((json) => Album.fromJson(json)).toList();
