bff35d92980e4a079424a43a9f426c52fb78a233
harry$ <harinarayananvm1692@gmail.com>
2020-05-14 10:26:55 +0530

    added address field

--- lib/Screens/CartScreen.dart
+++ lib/Screens/CartScreen.dart
@@ -59,21 +59,105 @@ class _CartScreenState extends State<CartScreen> {
             left: true,
           ),
           SizedBox(
+            height: 150,
+            width: MediaQuery.of(context).size.width,
+            child: Card(
+              color: Colors.greenAccent,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(150, 10, 10, 0),
+                      child: Text(
+                        'Address',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed:(){
+
+                        
+                      },
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          'Change',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 10, 0),
+                      child: Text(
+                        '',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                  ]),
+            ),
+          ),
+          SizedBox(
             height: 200,
             width: 400,
-            child: Padding(
-                padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-                child: Card(
-                  color: Colors.white70,
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(150, 10, 20, 0),
-                    child: Text(
-                      'Summary',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            child: Card(
+              color: Colors.white70,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(150, 10, 10, 0),
+                      child: Text(
+                        'Summary',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
                     ),
-                  ),
-                )),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 10, 0),
+                      child: Text(
+                        'Subtotal :',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 10, 0),
+                      child: Text(
+                        'Delivery Cost :',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 10, 0),
+                      child: Text(
+                        'Discount :',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 15, 10, 0),
+                      child: Text(
+                        'Total :',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.green),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                  ]),
+            ),
           ),
         ]));
   }
